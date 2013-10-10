c7a58a9e3820b845a3b02652ac7e64ef37019e53
IgorSusmelj <igor_7779@hotmai.com>
2013-10-10 21:30:28 +0200

    added service class

---
+++ SleepCalc/src/com/pps/sleepcalc/sensorService.java
@@ -0,0 +1,19 @@
+package com.pps.sleepcalc;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class sensorService extends IntentService{
+
+	public sensorService(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		//set up sensores 
+		
+	}
+
+}
