9e48db90a7bf736e7afdbac21ecb4f1511a0b4fe
IgorSusmelj <igor_7779@hotmai.com>
2013-11-10 00:36:24 +0100

    fixed Q, R flipped error

--- SleepClock/src/com/pps/sleepcalc/MainActivity.java
+++ SleepClock/src/com/pps/sleepcalc/MainActivity.java
@@ -73,8 +73,8 @@ public class MainActivity extends Activity implements TimePicker.OnTimeChangedLi
 		SharedPreferences settings = getSharedPreferences(SHARED_PREF_NAME,0);
 		triggerDelay.setText(settings.getString("triggerDelay", "1000"));
 		gyroSensorTrigger.setText(settings.getString("gyroSensorTrigger", "0.005"));
-		kalmanRnoise.setText(settings.getString("kalmanRnoise", "0.0125"));
-		kalmanQnoise.setText(settings.getString("kalmanQnoise", "80.0"));
+		kalmanRnoise.setText(settings.getString("kalmanRnoise", "80.0"));
+		kalmanQnoise.setText(settings.getString("kalmanQnoise", "0.0125"));
 
 		sensorPrecisionSwitch.setChecked(settings.getBoolean("sensorPrecisionSwitch", false));
 		
